f451403bdbd32da3f1f8b0d7e6e13df2f44686f4
FelipeAlves99 <falves99.dev@gmail.com>
2019-09-18 23:50:59 -0300

    Domain refactor

--- DDD_Project/DDD.Domain/Interfaces/IService/ItemServices/IUpdateItemService.cs
+++ DDD_Project/DDD.Domain/Interfaces/IService/ItemServices/IUpdateItemService.cs
@@ -1,9 +1,11 @@
 ﻿using DDD.Domain.Entities;
+using System.Collections.Generic;
 
 namespace DDD.Domain.Interfaces.IService.ItemServices
 {
     public interface IUpdateItemService
     {
         void UpdateItemService(Item item);
+        void UpdateStorageService(List<Item> item);
     }
 }
--- DDD_Project/DDD.Domain/Services/ItemService/UpdateItemService.cs
+++ DDD_Project/DDD.Domain/Services/ItemService/UpdateItemService.cs
@@ -1,4 +1,5 @@
-﻿using DDD.Domain.Entities;
+﻿using System.Collections.Generic;
+using DDD.Domain.Entities;
 using DDD.Domain.Interfaces.IRepository;
 using DDD.Domain.Interfaces.IService.ItemServices;
 
@@ -7,6 +8,17 @@ namespace DDD.Domain.Services.ItemService
     public class UpdateItemService : IUpdateItemService
     {
         private IItemRepository _itemRepository;
+
+        public UpdateItemService(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        public void UpdateStorageService(List<Item> item)
+        {
+            _itemRepository.Patchitem(item);
+        }
+
         void IUpdateItemService.UpdateItemService(Item item)
         {
             _itemRepository.PutItem(item);
