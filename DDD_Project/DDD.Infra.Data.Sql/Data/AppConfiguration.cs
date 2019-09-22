04b5a16d54137c110c9eb0d4f2a19b3f70cf57d9
FelipeAlves99 <falves99.dev@gmail.com>
2019-09-21 22:26:49 -0300

    Created the AppConfiguration class for connectionString config

---
+++ DDD_Project/DDD.Infra.Data.Sql/Data/AppConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DDD.Infra.Data.Sql.Data
+{
+    public class AppConfiguration
+    {
+        public readonly string _connectionString = string.Empty;
+        public AppConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            var root = configurationBuilder.Build();
+            _connectionString = root.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value;
+            var appSetting = root.GetSection("ApplicationSettings");
+        }
+
+        public string ConnectionString
+        {
+            get => _connectionString;
+        }
+
+    }
+}
