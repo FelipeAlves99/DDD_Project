046a6c45677f8629d98ff203d9665b1eca9c58ba
FelipeAlves99 <falves99.dev@gmail.com>
2019-09-30 23:39:32 -0300

    Company repository creation

---
+++ DDD_Project/DDD.Infra.Data.Sql/Repository/CompanyRepository.cs
@@ -0,0 +1,77 @@
+﻿using DDD.Domain.Entities.CompanyEntity;
+using DDD.Domain.Interfaces.IRepository;
+using DDD.Infra.Data.Sql.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DDD.Infra.Data.Sql.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private DDDProjectContext _context = new DDDProjectContext();
+
+        public CompanyRepository(DDDProjectContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteCompany(int id)
+        {
+            _context.Set<Company>().Remove(GetCompanyById(id));
+            _context.SaveChanges();
+        }
+
+        public IList<Company> GetAllCompany()
+        {
+            return _context.Companies
+                .AsNoTracking()
+                .Include(c => c.Atividade_Principal)
+                .Include(c => c.Atividades_Secundarias)
+                .ToList();
+        }
+
+        public Company GetCompanyByCnpj(string cnpj)
+        {
+            return _context.Companies
+                .AsNoTracking()
+                .Where(c => c.Cnpj == cnpj)
+                .Include(c => c.Atividade_Principal)
+                .Include(c => c.Atividades_Secundarias)
+                .FirstOrDefault();
+        }
+
+        public Company GetCompanyById(int id)
+        {
+            return _context.Companies
+                .AsNoTracking()
+                .Where(c => c.Id == id)
+                .Include(c => c.Atividade_Principal)
+                .Include(c => c.Atividades_Secundarias)
+                .FirstOrDefault();
+        }
+
+        public Company GetCompanyByName(string name)
+        {
+            return _context.Companies
+                .AsNoTracking()
+                .Where(c => c.Nome == name)
+                .Include(c => c.Atividade_Principal)
+                .Include(c => c.Atividades_Secundarias)
+                .FirstOrDefault();
+        }
+
+        public void PostCompany(Company company)
+        {
+            _context.Set<Company>().Add(company);
+            _context.SaveChanges();
+        }
+
+        public void PutCompany(Company company)
+        {
+            _context.Attach(company);
+            _context.Entry(company).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
