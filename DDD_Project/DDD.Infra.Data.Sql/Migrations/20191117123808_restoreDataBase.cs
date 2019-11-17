84c47f9c463c94fa7c9d8ae1f48b6efcdede0761
FelipeAlves99 <falves99.dev@gmail.com>
2019-11-17 09:39:34 -0300

    Restored database

---
+++ DDD_Project/DDD.Infra.Data.Sql/Migrations/20191117123808_restoreDataBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DDD.Infra.Data.Sql.Migrations
+{
+    public partial class restoreDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
